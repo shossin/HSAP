1f5ff9cf326f0138fdaf8a381f50fe9aee430725
Khan Safayet Hossin <safayet.hossin@gmail.com>
2022-11-13 15:09:58 +1300

    adding forms

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,18 +9,30 @@ import { PropertyCardComponent } from './property/property-card/property-card.co
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from './services/housing.service';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
+import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
 
+const appRoutes: Routes =[
+  {path:'add-property', component: AddPropertyComponent},
+  {path:'rent-property', component: PropertyListComponent},
+  {path:'', component: PropertyListComponent},
+  {path:'property-detail/:id', component: PropertyDetailComponent},
+  {path:'**', component: PropertyListComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
     PropertyCardComponent,
     PropertyListComponent,
-      NavBarComponent
+    NavBarComponent,
+    AddPropertyComponent,
+    PropertyDetailComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [HousingService],
   bootstrap: [AppComponent]
--- Frontend/src/app/services/housing.service.ts
+++ Frontend/src/app/services/housing.service.ts
@@ -12,15 +12,16 @@ export class HousingService {
 
 constructor(private http:HttpClient) { }
 
-getAllProperties(): Observable<IProperty[]>{
+getAllProperties(SellRent: number): Observable<IProperty[]>{
   return this.http.get("data/properties.json")
   .pipe(
     map(data=>{
       const propertiesArray: Array<IProperty>=[]; //array initialize
       const sdata = JSON.parse(JSON.stringify(data));
       for (const id in sdata) {
-        if (sdata.hasOwnProperty(id)) {
+        if ((sdata.hasOwnProperty(id))&&(sdata[id].SellRent ===SellRent)) {
           const element = sdata[id];
+
           propertiesArray.push(element);
         }
       }
